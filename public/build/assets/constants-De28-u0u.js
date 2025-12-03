2cd7cfdf65ea1c92348c7242af0dc44d0d2e4ada
danielecheveste <daniel.echeveste@maresalogistica.com>
2025-12-03 16:45:40 +0000

    build

---
+++ public/build/assets/constants-De28-u0u.js
@@ -0,0 +1,1 @@
+import{a8 as r}from"./events-776716bd.esm-C3d9VITL.js";const e=parseInt(r.replace(/\D+/g,""));export{e as v};
--- public/build/assets/constants-DwhEF5et.js
+++
@@ -1,1 +0,0 @@
-import{a9 as r}from"./events-776716bd.esm-Cffp_Zz7.js";const e=parseInt(r.replace(/\D+/g,""));export{e as v};
